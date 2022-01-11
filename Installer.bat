46159d3f8793f40c246106d94733cee5e997e75b
ShadowOcto <80975779+ShadowOcto@users.noreply.github.com>
2022-01-11 17:18:35 +0000

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -3,3 +3,4 @@ title Shadow's Installer
 set /p program="Installer>Install "
 
 curl https://shadowocto.github.io/Installer/Main.raw> C:\temp\Installer.bat && start C:\temp\Installer.bat %program% && timeout /t 1 && del C:\temp\Installer.bat
+exit
